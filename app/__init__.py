3cf50a9e27a75f84f07607dbd958938158a7d565
meyash <yashsn2127@gmail.com>
2020-05-01 00:58:40 +0530

    clutter

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,10 @@ def index():
         # else:
         return render_template("login.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
