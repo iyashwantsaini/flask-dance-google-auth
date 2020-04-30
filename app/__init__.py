5d316c53f7e2309457f789c0c2655e5938d9ebe8
meyash <yashsn2127@gmail.com>
2020-04-30 23:38:48 +0530

    dash

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, url_for, flash, render_template
 from flask_login import login_required, logout_user
 from .config import Config
 from .models import db, login_manager
-from .oauth import blueprint
+from .oauth import blueprint, google_logged_in
 from .cli import create_db
 
 app = Flask(__name__)
@@ -21,4 +21,16 @@ def logout():
 
 @app.route("/")
 def index():
-    return render_template("login.html")
+        if google_logged_in:
+            return render_template("dashboard.html")
+        else:
+            return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
